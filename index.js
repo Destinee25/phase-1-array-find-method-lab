9f113a180b77d69b4308116502bfc847d7dcd46b
SDBoahen <SDBoahen@gmail.com>
2021-07-21 17:45:46 -0400

    End of 7-21-2021 0719-B-PHASE1

--- index.js
+++ index.js
@@ -1,1 +1,187 @@
+console.log("YO SAM! REMEBER TO PUSH THIS UP TO GITHUB FOR FOLX")
 
+
+
+
+document.addEventListener("click", (event)=>{console.log(event.target) })
+// document.addEventListener("click", function(event){console.log(event.target) })
+
+const h1AtTheTop = document.querySelector("h1")
+    console.log(h1AtTheTop)  //
+    console.log(h1AtTheTop.innerText)  //
+
+    h1AtTheTop.innerText = "WHAT'S GOING ON WONDERFUL PEOPLEEEE!!!!🙌🏾 "
+    console.log(h1AtTheTop.innerText)  //
+
+// NONO : h1 :: Reserve Word
+
+
+// Selectors
+//// querySelector
+//// getElementById
+//// getElementByClassName
+
+
+
+
+
+
+
+// console.log("Yurrrr!")
+
+
+// let number = 7
+//     console.log(number)
+
+
+// let names = ["Sam", "Ray", "John"]
+//     // console.log(names)
+
+
+// const takeInNamesAF =(n)=>{ // paraMeters - paranthesis  
+//         console.log(n)
+//         console.log(n[0])
+
+
+//         let something = names[0]
+//         console.log(something)
+
+// }
+
+// takeInNamesAF(names)
+// // (names)  <- argument .../helpful reminder on the way 😅👍
+
+// // function takeInNamesRF(n){
+
+// // }
+
+
+
+
+
+
+
+
+const sam ={
+
+    name: "sam",
+    hometown: "THEBOOGIEDOWNBRONX",
+    sort: "SORT",
+
+        // function sort(){ 
+        //     console.log("lolol") 
+        // }
+    
+}
+// THAT'S A NO-NO  sam.sort()
+// THAT'S A NO-NO  >>  object
+//// "object" could be a Reserve Word in certain scopes
+/////// other words and terms can be a Reserve Word in certain scopes as well
+
+
+
+
+// const people ={
+
+//     sam: { name: "Sam", hometown: "BX" }
+
+//     // THAT'S A NONO  >> let j = { watv: "LOOP"}
+
+// } // People Object
+// //// Object Have Key:Value Pairs  :)   
+//     console.log("THIS IS THE PEOPLE OBJECT:    ", people) 
+//         console.log(people.sam)
+        
+
+
+
+
+// const ppl =[
+
+//     { name: "Sam", hometown: "BX" }
+
+// ] // People Array
+// ////  Arrays Have Indexes
+//     console.log("THIS IS THE PEOPLE ARRAY:    ", ppl) 
+//         console.log("[0]  ",  ppl[0])
+
+//         console.log("something 👀")
+//         console.log(ppl)
+//         console.log("something 👀", ppl)
+//         console.log(ppl, ppl[0], ppl)
+
+ 
+
+
+
+
+
+
+//// REMEMBER.... "Who Would Win in a Fight"
+// <>
+// ()
+// {}
+// []
+
+// ({ })
+
+// , ; }
+//// Pay Attention To 
+
+
+
+
+// Syntax 
+//// BEYOND ME - THAT'S JUST HOW IT IS 🤷🏾‍♂️
+
+// Convention
+//// That's How You'll Come Across it Most Often 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Yurrr")
+
+
+// theTeam = [
+
+//     { name: "Sam", hometown: "The BoogieDown Bronx" }
+
+// ]
+// console.log(theTeam)
