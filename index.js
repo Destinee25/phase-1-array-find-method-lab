58848dba5aa8c0aefb602bcc2bdbb81de67ca098
Destinee Robinson <Destineerobinson08@gmail.com>
2021-07-22 16:54:24 -0400

    commit tree 000d9fbd72077499902ad1e1caa1e10c164212e6

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+function superbowlWin(record){
+    const foundSeason = record.find(season => season.result === "W")
+    if(foundSeason)
+    return foundSeason.year
+}
+//This was a little difficult may need to review
